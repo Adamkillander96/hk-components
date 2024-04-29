f6b9e8017b8c9c2133d7d3237b2437c02b798a79
Adam Killander <34036252+Adamkillander96@users.noreply.github.com>
2024-04-29 16:15:12 +0200

    chore: fixed naming

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: 'killander',
+  namespace: 'hagshult',
   validatePrimaryPackageOutputTarget: true,
   outputTargets: [
     {
